d0922366caacb07b07ab5921c43f5db913016788
Jonathan Huinca <jhuinca2020@alu.uct.cl>
2024-04-27 21:46:42 -0400

    Prueba de Muestra de datos relacionados por un array
    se muestra tiendas favoritas de un usuario en especifico, pero la cual el dato de los id de tiendas favoritas están en un array de array

--- pythonMongoDB/MostrarDatosRelacionadosEnArreglo.py
+++ pythonMongoDB/MostrarDatosRelacionadosEnArreglo.py
@@ -1,10 +1,9 @@
 import datetime
-from pymongo import MongoClient # type: ignore
+from pymongo import MongoClient 
 from bson import ObjectId
 
-
 #=================================================================================
-#=====Relacion un usario con un arreglo objetos de id referenciados a un tienda===
+#=====Relacion un usario con un array de array de id referenciados a un tienda===
 #=================================================================================
 
 try:
@@ -13,22 +12,35 @@ try:
     collection = database['usuarios']#Nombre de la coleccion a utilzar 
     proc = database['productos']#Coleccion productos a usar
     
-    documents = collection.find({"nombre":"miguel"})#consulta sobre colecciones con nombre=miguel  
+    documents = collection.find({"nombre":"Fermin"})#consulta sobre colecciones con nombre=miguel  
     #-------Mostrar todas los datos de productos de usuario-----
     print("------Mostrar cada uno de los favoritos de del usuario miguel -----")
-    # print("primero: ",documents)
     
+    # list_documents=list(documents["favoritos"])
+    # print("primero1: ",list_documents)
+    
+    #print(documents)
     # for md in documents:#Muestra los usuarios de nombre miguel con sus datos
     #     print("miguel",md)
     
+    
+    print("primero",documents)
+    
+    
     for d in documents:#Muesta el arreglo de los id de favoritos
-        a= d["favoritos"]
-        print(a)
-
-    for x in a:#Muestra uno por uno de los datos de favorito(de cada producto)
-        z=proc.find({"_id":ObjectId(x)})
-        for ll in z:
-            print(ll)
+        print(d["favoritos"])
+        a=d["favoritos"]
+        
+    # print("datos:: ", a[0][0])
+    # print("datos:: ", a[1][0])
+    
+    for i in range(len(a)):
+        aa=a[i][0]
+        print(aa,"----------",type(aa))
+        r = proc.find({"_id":ObjectId(aa) })
+        
+        for j in r:#Se hace el for para mostra cada uno de los datos
+            print(j)
         
 except Exception as ex:
     print("Error durante la conexión {}".format(ex))
