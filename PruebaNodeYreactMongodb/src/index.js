63ab9634d091035d761372d24a65b5615980f9a0
Jonathan Huinca <jhuinca2020@alu.uct.cl>
2024-06-07 21:02:07 -0400

    loginRegister

--- PruebaNodeYreactMongodb/src/index.js
+++
@@ -1,8 +0,0 @@
-import app from './app.js' 
-import { connectDB } from './db.js' //se usa el import asi por que solo se uso un solo "export"
-
-connectDB();
-
-app.listen(4000);
-console.log('Servidor en el puerto', 4000);
-
